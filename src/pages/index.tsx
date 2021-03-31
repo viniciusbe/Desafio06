5da1530d125526df16d07abebb4a5da6fbf69cb3
viniciusbe <vinicius.bernardesds@gmail.com>
2021-03-31 15:26:18 -0300

    finish challenge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -82,7 +82,13 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
                 <div>
                   <time>
                     <FiCalendar />
-                    {post.first_publication_date}
+                    {format(
+                      new Date(post.first_publication_date),
+                      'dd MMM yyyy',
+                      {
+                        locale: ptBR,
+                      }
+                    )}
                   </time>
                   <address>
                     <FiUser />
@@ -114,16 +120,12 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
+  console.log(postsResponse.results[0].first_publication_date);
+
   const posts = postsResponse.results.map(post => {
     return {
       uid: post.uid,
-      first_publication_date: format(
-        new Date(post.first_publication_date),
-        'dd MMM yyyy',
-        {
-          locale: ptBR,
-        }
-      ),
+      first_publication_date: post.first_publication_date,
       data: {
         title: post.data.title,
         subtitle: post.data.subtitle,
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -4,20 +4,23 @@ import Head from 'next/head';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
 
-import { FiCalendar, FiUser } from 'react-icons/fi';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import { useRouter } from 'next/router';
 import commonStyles from '../../styles/common.module.scss';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './post.module.scss';
 import Header from '../../components/Header';
 
 interface Post {
+  uid: string;
   first_publication_date: string | null;
   data: {
     title: string;
+    subtitle: string;
     banner: {
       url: string;
     };
@@ -36,6 +39,8 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
   // const readingTime = post.data.content.reduce(group =>
 
   //   , [])
@@ -46,26 +51,45 @@ export default function Post({ post }: PostProps): JSX.Element {
         <title>{post.data.title} | spacetraveling</title>
       </Head>
       <Header />
-      <section className={styles.container}>
-        <figure className={styles.bannerContainer}>
-          <img src={post.data.banner.url} alt="logo" />
-        </figure>
-        <main className={styles.content}>
-          <h1>{post.data.title}</h1>
-          <div>
-            <time>
-              <FiCalendar />
-              {post.first_publication_date}
-            </time>
-            <address>
-              <FiUser />
-              {post.data.author}
-            </address>
-            <time />
-          </div>
-          <article />
-        </main>
-      </section>
+      {router.isFallback ? (
+        <p>Carregando...</p>
+      ) : (
+        <section className={styles.container}>
+          <figure className={styles.bannerContainer}>
+            <img src={post.data.banner.url} alt="banner" />
+          </figure>
+          <main className={styles.content}>
+            <h1>{post.data.title}</h1>
+            <div>
+              <time>
+                <FiCalendar />
+                {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+                  locale: ptBR,
+                })}
+              </time>
+              <address>
+                <FiUser />
+                {post.data.author}
+              </address>
+              <time>
+                <FiClock />4 min
+              </time>
+            </div>
+            <article className={styles.article}>
+              {post.data.content.map(group => (
+                <div key={group.heading}>
+                  <h2>{group.heading}</h2>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: RichText.asHtml(group.body),
+                    }}
+                  />
+                </div>
+              ))}
+            </article>
+          </main>
+        </section>
+      )}
     </>
   );
 }
@@ -81,7 +105,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   );
 
   return {
-    paths: [{ params: { slug: posts.results[0].uid } }],
+    paths: posts.results.map(post => ({ params: { slug: post.uid } })),
     fallback: true,
   };
 };
@@ -100,27 +124,20 @@ export const getStaticProps: GetStaticProps = async context => {
   // console.log(data.content[0]);
 
   const post = {
-    first_publication_date: format(
-      new Date(first_publication_date),
-      'dd MMM yyyy',
-      {
-        locale: ptBR,
-      }
-    ),
+    uid,
+    first_publication_date,
     data: {
       title: data.title,
+      subtitle: data.subtitle,
       banner: {
         url: data.banner.url,
       },
       author: data.author,
-      content: data.content.map(group => ({
-        heading: group.heading,
-        body: RichText.asText(group.body),
-      })),
+      content: data.content,
     },
   };
 
-  console.log(JSON.stringify(post, null, 2));
+  console.log(JSON.stringify(data, null, 2));
 
   return {
     props: { post },
